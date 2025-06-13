62d73b2e8d6e8b7d85144ecace551164ed118562
Hanna <hanna@prevetfamily.com>
2025-06-13 15:37:02 +0200

    Create components for home section and footer content

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer className="text-center bg-color-bg-secondary p-10">
+      <p className="text-xl ">Footer section</p>
+    </footer>
+  );
+};
+
+export default Footer;
