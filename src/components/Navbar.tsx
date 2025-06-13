44fd3bc55e446789baa03778135965bf639c60a4
Hanna <hanna@prevetfamily.com>
2025-06-13 15:35:41 +0200

    Use colors and fonts in Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -11,21 +11,24 @@ export default function Navbar() {
   const [menuOpen, setMenuOpen] = useState(false);
 
   return (
-    <nav className="w-full bg-teal-600" aria-label="Main navigation">
+    <nav className="w-full" aria-label="Main navigation">
       <div className="max-w-7xl mx-auto py-5 px-5 grid grid-cols-2 lg:grid-cols-3 items-center">
         {/* Logo text */}
-        <Link to="/" className="text-2xl tracking-tighter text-white">
+        <Link
+          to="/"
+          className="text-2xl md:text-4xl tracking-tighter font-light font-serif xl:py-2"
+        >
           Park Explorer
         </Link>
 
         {/* Desktop links */}
         <ul className="hidden lg:flex justify-center gap-8">
           {links.map((link) => (
-            <li key={link.to}>
+            <li key={link.to} className="">
               <NavLink
                 to={link.to}
                 className={({ isActive }) =>
-                  `text-white ${isActive ? "underline" : ""}`
+                  ` ${isActive ? "underline" : ""} p-2`
                 }
               >
                 {link.name}
@@ -38,8 +41,8 @@ export default function Navbar() {
         <div className="flex justify-end items-center gap-4 col-start-3">
           <button
             type="button"
-            className="hidden lg:inline text-white bg-green-900 px-4 py-2 rounded hover:bg-green-800 
-              focus:outline-none focus:ring-2 focus:ring-white"
+            className="hidden lg:inline text-white bg-color-button-primary px-4 py-2 rounded-3xl 
+            hover:bg-green-800 focus:outline-teal-600"
           >
             Future Button
           </button>
@@ -47,7 +50,7 @@ export default function Navbar() {
           {/* Mobile menu button */}
           <button
             onClick={() => setMenuOpen(!menuOpen)}
-            className="lg:hidden text-white text-xl"
+            className="lg:hidden  text-xl"
             aria-label="Open menu"
           >
             Menu
@@ -57,7 +60,7 @@ export default function Navbar() {
 
       {/* Mobile navlink menu */}
       {menuOpen && (
-        <div className="lg:hidden px-4 pb-4 animate-slide-fade">
+        <div className="lg:hidden px-5 pb-4 animate-slide-fade">
           <ul className="flex flex-col gap-4">
             {links.map((link) => (
               <li key={link.to}>
@@ -65,7 +68,7 @@ export default function Navbar() {
                   to={link.to}
                   onClick={() => setMenuOpen(false)}
                   className={({ isActive }) =>
-                    `block text-white ${isActive ? "underline" : ""}`
+                    `block  ${isActive ? "underline" : ""}`
                   }
                 >
                   {link.name}
