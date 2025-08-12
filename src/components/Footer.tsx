250a0f411b676d3d5ee166b01f11d6ca022e896c
Hanna <hanna@prevetfamily.com>
2025-08-12 12:42:09 +0200

    Create wave styling in footer component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,63 @@
+import { Link, NavLink } from "react-router-dom";
+
 const Footer = () => {
+  const links = [
+    { name: "Home", to: "/" },
+    { name: "Parks", to: "/parks" },
+  ];
+
   return (
-    <footer className="text-center bg-color-bg-secondary p-10">
-      <p className="text-xl ">Footer section</p>
+    <footer className="relative text-center bg-color-button-primary pt-16">
+      {/* Wave svg */}
+      <div
+        className="absolute top-0 left-0 w-full overflow-hidden 
+      leading-none"
+      >
+        <svg
+          aria-hidden="true"
+          className="w-full h-auto transform rotate-y-180"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 1200 120"
+          preserveAspectRatio="none"
+        >
+          <path
+            d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,
+            168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,
+            18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,
+            321.39,56.44Z"
+            className="fill-[var(--color-bg)]"
+          />
+        </svg>
+      </div>
+
+      {/* Footer content */}
+      <section className="relative flex flex-col items-center py-8 md:py-15">
+        <Link
+          to="/"
+          className="text-2xl md:text-3xl tracking-tighter font-light font-serif
+          xl:py-2 text-white mb-4"
+        >
+          Park Explorer
+        </Link>
+        <p className="text-sm font-serif text-white  mb-5">
+          Made with curiosity and joy by Hanna.
+        </p>
+        <nav aria-label="Navigation links">
+          <ul className="flex flex-col sm:flex-row gap-6">
+            {links.map((link) => (
+              <li key={link.to}>
+                <NavLink
+                  to={link.to}
+                  className="text-white font-serif text-lg underline-offset-2 
+                sm:text-base hover:underline focus:underline transition"
+                >
+                  {link.name}
+                </NavLink>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </section>
     </footer>
   );
 };
