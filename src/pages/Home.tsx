fd70cf5cc76fe489820bcb534f99070769fa8196
Hanna <hanna@prevetfamily.com>
2025-08-12 12:43:41 +0200

    Fix width styling in home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,7 +4,7 @@ const Home = () => {
   return (
     <>
       <HomeHero />
-      <p className="text-center mt-5">Home page content</p>
+      <p className="text-center mt-5 py-15">Home page content</p>
     </>
   );
 };
