c8e4bda81e85d6bf7cd36cac9f2d2814d8120dba
Hanna <hanna@prevetfamily.com>
2025-08-13 14:15:01 +0200

    Update park api request to only allow a few park designations to limit response

--- src/api/parks.ts
+++ src/api/parks.ts
@@ -7,7 +7,7 @@ const apiUrl = import.meta.env.VITE_NPS_PARKS_BASE_URL;
 // Fetch parks
 export async function fetchParks(): Promise<Park[]> {
   const url = new URL(apiUrl);
-  url.searchParams.set("limit", "40");
+  url.searchParams.set("limit", "474");
   url.searchParams.set("api_key", apiKey);
 
   const response = await fetch(url.toString());
@@ -17,34 +17,47 @@ export async function fetchParks(): Promise<Park[]> {
 
   const data = await response.json();
 
-  const parks: Park[] = data.data.map((item: ApiPark) => ({
-    id: item.id,
-    fullName: item.fullName,
-    parkCode: item.parkCode,
-    description: item.description,
-    activities: Array.isArray(item.activities)
-      ? (item.activities as Activity[])
-      : [],
-    topics: Array.isArray(item.topics) ? (item.topics as Topic[]) : [],
-    states: item.states,
-    directionsInfo: item.directionsInfo,
-    images: Array.isArray(item.images) ? (item.images as ParkImage[]) : [],
-    weatherInfo: item.weatherInfo,
-    name: item.name,
-    designation: item.designation,
-    addresses: Array.isArray(item.addresses)
-      ? (item.addresses as Address[])
-      : [],
-    operatingHours: item.operatingHours ?? [],
-    contacts:
-      typeof item.contacts === "object"
-        ? item.contacts
-        : { phoneNumbers: [], emailAddresses: [] },
-  }));
+  const allowedDesignations = [
+    "national park",
+    "national preserve",
+    "national seashore",
+  ];
+
+  const parks: Park[] = data.data
+    .filter(
+      (item: ApiPark) =>
+        item.designation &&
+        allowedDesignations.some((designation) =>
+          item.designation.toLowerCase().includes(designation)
+        )
+    )
+    .map((item: ApiPark) => ({
+      id: item.id,
+      fullName: item.fullName,
+      parkCode: item.parkCode,
+      description: item.description,
+      activities: Array.isArray(item.activities)
+        ? (item.activities as Activity[])
+        : [],
+      topics: Array.isArray(item.topics) ? (item.topics as Topic[]) : [],
+      states: item.states,
+      directionsInfo: item.directionsInfo,
+      images: Array.isArray(item.images) ? (item.images as ParkImage[]) : [],
+      weatherInfo: item.weatherInfo,
+      name: item.name,
+      designation: item.designation,
+      addresses: Array.isArray(item.addresses)
+        ? (item.addresses as Address[])
+        : [],
+      operatingHours: item.operatingHours ?? [],
+      contacts:
+        typeof item.contacts === "object"
+          ? item.contacts
+          : { phoneNumbers: [], emailAddresses: [] },
+    }));
 
   return parks;
 }
-
 // Fetch single park by parkCode
 export async function fetchPark(parkCode: string): Promise<Park> {
   const url = new URL(apiUrl);
