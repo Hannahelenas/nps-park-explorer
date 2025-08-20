5d6da4499c0488b08bb8fdc5555f7feb6d4a18f2
Hanna <hanna@prevetfamily.com>
2025-08-20 11:08:22 +0200

    Display breadcrumbs in layot depending on route length

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,13 +1,19 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
 import ScrollHandler from "./ScrollHandler";
+import Breadcrumbs from "./Breadcrumbs";
 
 export default function Layout() {
+  const location = useLocation();
+  const pathnames = location.pathname.split("/").filter((x) => x);
+  const showBreadcrumbs = pathnames.length >= 2;
+
   return (
     <>
       <ScrollHandler />
       <Navbar />
+      {showBreadcrumbs && <Breadcrumbs />}
       <main className="">
         <Outlet />
       </main>
