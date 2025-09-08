876f00580a6edc6a4deb96326e505fd40795237d
Hanna <hanna@prevetfamily.com>
2025-09-08 20:48:53 +0200

    Add weather data in park weather section

--- src/components/ParkWeatherSection.tsx
+++ src/components/ParkWeatherSection.tsx
@@ -1,5 +1,7 @@
 import type { Park } from "../types/Park";
 import { parseLatLong } from "../utils/parseCoordinates";
+import { getWeatherIcon } from "../utils/getWeatherIcon";
+import { useWeather } from "../hooks/useWeather";
 
 interface ParkWeatherSectionProps {
   park: Park;
@@ -7,30 +9,56 @@ interface ParkWeatherSectionProps {
 
 const ParkWeatherSection = ({ park }: ParkWeatherSectionProps) => {
   const coordinates = park.latLong ? parseLatLong(park.latLong) : null;
+  const { weather, loading, error } = useWeather(
+    coordinates?.lat,
+    coordinates?.long
+  );
+
   return (
     <section className="max-w-6xl mx-auto px-5 md:px-10 xl:px-5">
-      <h2
-        className="text-2xl md:text-4xl font-black tracking-tighter mb-6 
-      mt-6"
-      >
+      <h2 className="text-2xl md:text-4xl font-black tracking-tighter mb-6 mt-6">
         Weather and climate
       </h2>
-      <p className="mt-2 font-serif leading-relaxed">{park.weatherInfo}</p>
-      <article className=" rounded-3xl">
-        <h3
-          className="text-2xl md:text-3xl font-black tracking-tighter mb-4 
-      mt-4"
-        >
-          Current weather
-        </h3>
-        {coordinates ? (
-          <p>
-            Latitude: {coordinates.lat}, Longitude: {coordinates.long}
-          </p>
-        ) : (
-          <p>Coordinates not available</p>
-        )}
-      </article>
+
+      <div className="flex flex-col md:flex-row md:items-start md:gap-8">
+        <p className="font-serif leading-relaxed md:flex-1">
+          {park.weatherInfo}
+        </p>
+        <article className="rounded-3xl mt-4 md:mt-0 p-5 bg-white md:w-1/3">
+          <h4 className="text-2xl md:text-3xl font-black tracking-tighter">
+            Current weather
+          </h4>
+
+          {loading && <p>Loading weather...</p>}
+          {error && <p className="">Error: {error}</p>}
+
+          {weather && (
+            <div className="flex flex-col items-start">
+              <p className="font-serif mt-2">{weather.localTime}</p>
+
+              <div
+                className="flex items-center justify-between gap-6 mt-2 
+              mb-2"
+              >
+                <p className="text-3xl font-black">
+                  {Math.round(weather.temp)}°C
+                </p>
+                {getWeatherIcon(weather.main)}
+              </div>
+
+              <p className="mt-2 font-serif">
+                {weather.description.charAt(0).toUpperCase() +
+                  weather.description.slice(1)}
+              </p>
+
+              <p className="font-serif">
+                Feels like: {Math.round(weather.feelsLike)}°C /{" "}
+                {Math.round((weather.feelsLike * 9) / 5 + 32)}°F
+              </p>
+            </div>
+          )}
+        </article>
+      </div>
     </section>
   );
 };
