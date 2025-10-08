ebbc5fbfb5a73af76a11c128035cb8a03d555120
Hanna <hanna@prevetfamily.com>
2025-10-08 12:24:32 +0200

    Change weather icon color and size in get wheater icon function

--- src/utils/getWeatherIcon.tsx
+++ src/utils/getWeatherIcon.tsx
@@ -1,28 +1,28 @@
 import {
-  IoSunny,
   IoRainy,
   IoSnow,
   IoCloudy,
   IoThunderstorm,
+  IoSunnyOutline,
 } from "react-icons/io5";
 import { MdOutlineCloud } from "react-icons/md";
 import type { JSX } from "react/jsx-runtime";
 
-const iconSize = "text-7xl";
+const iconSize = "text-4xl";
 
 const weatherIcons: Record<string, JSX.Element> = {
-  clear: <IoSunny className={`text-yellow-500 ${iconSize}`} />,
-  clouds: <IoCloudy className={`text-gray-500 ${iconSize}`} />,
-  rain: <IoRainy className={`text-blue-500 ${iconSize}`} />,
-  drizzle: <MdOutlineCloud className={`text-blue-500 ${iconSize}`} />,
-  thunderstorm: <IoThunderstorm className={`text-purple-600 ${iconSize}`} />,
-  snow: <IoSnow className={`text-blue-500 ${iconSize}`} />,
+  clear: <IoSunnyOutline className={`${iconSize}`} />,
+  clouds: <IoCloudy className={`${iconSize}`} />,
+  rain: <IoRainy className={`${iconSize}`} />,
+  drizzle: <MdOutlineCloud className={`${iconSize}`} />,
+  thunderstorm: <IoThunderstorm className={`${iconSize}`} />,
+  snow: <IoSnow className={`${iconSize}`} />,
 };
 
 export function getWeatherIcon(main: string): JSX.Element {
   return (
     weatherIcons[main.toLowerCase()] ?? (
-      <MdOutlineCloud className={`text-gray-500 ${iconSize}`} />
+      <MdOutlineCloud className={`${iconSize}`} />
     )
   );
 }
